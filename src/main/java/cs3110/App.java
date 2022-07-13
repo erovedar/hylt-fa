8ee616e3f1bd9168c67348998143b504449456db
Ero V <erovdr@gmail.com>
2022-07-13 15:04:38 -0700

    read-in file

--- src/main/java/cs3110/App.java
+++ src/main/java/cs3110/App.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 
 public class App 
 {
-    static String filePath ="";
-    static int n;
-    static Boolean[] finalStates;
-    static String[] az;
-    static ArrayList<Integer[]> trstn = new ArrayList<>();
-    static String[] tests; 
+    static String filePath = "src/main/java/cs3110/test.txt";
+
+    static int n;   // number of states
+    static Boolean[] finalStates;   // array of final/accepting states
+    static String[] az; // array of symbols in alphabet
+    static ArrayList<Integer[]> trstn = new ArrayList<>();  // list of transitions
+    static String[] tests;  // array of tests
 
     public static void main( String[] args )
     {
@@ -20,8 +21,21 @@ public class App
         System.out.println("1) Number of states: " + n);
         System.out.println("2) final states: " + printFin(finalStates));
         System.out.println("3) Alphabet: ");
+
+        System.out.println("4) Transitions: ");
+        System.out.println("5) Strings: ");
     }
 
+    /* Sample text file:
+     * 3        number of states
+     * 0 1      accepting state(s), whitespace deliminated
+     * 0 1      alphabet, whitespace deliminated
+     * (0 0 0)  transitions, new line deliminated
+     * (0 1 1)
+     * (1 0 0)
+     * (1 1 1)
+     * 1000     test cases, new line deliminated
+     * 10001    */
     public static void readFile(String filePath){
         try{
             File file = new File(filePath);
@@ -38,7 +52,24 @@ public class App
 
             String alphabet = sc.nextLine();
             az = alphabet.split("\\s+");
+            for (String s : az){
+                if(s.length() > 1){
+                    System.out.println("Error: alphabet contains multicharacter symbols");
+                    System.exit(1);
+                }
+            }
 
+            char openPar = '(';
+            char closePar = ')';
+            while(sc.hasNext()){
+                String next = sc.nextLine();
+                if(next.charAt(0) == openPar && next.charAt(next.length()-1) == closePar){
+                    
+                }
+                else{
+
+                }
+            }
             sc.close();
         }
         catch(Exception e){
@@ -46,6 +77,7 @@ public class App
             e.printStackTrace();
         }
     }
+
 
     public static String printFin(Boolean[] fin){
         String s = "";
@@ -57,7 +89,13 @@ public class App
         return s;
     }
 
-    public static String printAZ(ArrayList<Character> alph){
+    public static String printArr(String[] a){
+        String s = "";
+        
+        return s;
+    }
+
+    public static String printAZ(ArrayList alph){
         String s = "";
         for(int i = 0; i<alph.size()-1; i++){
             s += alph.get(i) + ", ";
